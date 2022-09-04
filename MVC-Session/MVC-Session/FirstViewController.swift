85805fbf25a49b739144356badaff3cbf52a9b35
feldblume5263 <feldblume5263@gmail.com>
2022-09-05 01:26:35 +0900

    update code

---
+++ MVC-Session/MVC-Session/FirstViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  MVC-Session
+//
+//  Created by Noah Park on 2022/09/03.
+//
+
+import UIKit
+
+final class FirstViewController: UIViewController {
+    
+    @IBOutlet weak private var forwardButton: UIButton!
+    
+    override func loadView() {
+        super.loadView()
+        print("firstView: load view")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("firstView: viewDidLoad")
+        setForwardButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("firstView: viewWillAppear")
+//        forwardButton.frame = CGRect(x: 0, y: 0, width: 200, height: 60)
+//        print(forwardButton.frame.size)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("firstView: viewWillLayoutSubviews")
+//        print(forwardButton.frame.size)
+    }
+
+    override func viewDidLayoutSubviews() {
+        print("firstView: viewDidLayoutSubviews")
+//        print(forwardButton.frame.size)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("firstView: viewDidAppear")
+//        print(forwardButton.frame.size)
+//        forwardButton.frame = CGRect(x: 0, y: 0, width: 200, height: 60)
+//        print(forwardButton.frame.size)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("firstView: viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("firstView: viewDidDisappear")
+    }
+}
+
+private extension FirstViewController {
+    func setForwardButton() {
+        forwardButton.setTitle("Next", for: .normal)
+        forwardButton.addTarget(self, action: #selector(forwardButtonAction), for: .touchUpInside)
+    }
+    
+    @objc func forwardButtonAction(_ sender: UIButton) {
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "secondView") {
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+}
+
