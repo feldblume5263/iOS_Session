ccde4b14aff38f94345d53b9a232d708048c624b
feldblume5263 <feldblume5263@gmail.com>
2022-09-04 23:39:48 +0900

    update code

---
+++ MVC-Session/MVC-Session/NewEntryPoint.swift
@@ -0,0 +1,16 @@
+//
+//  NewEntryPoint.swift
+//  MVC-Session
+//
+//  Created by Noah Park on 2022/09/04.
+//
+
+import Foundation
+
+//@main
+class NewEntryPoint {
+    
+    static func main() {
+        print("하나, 둘, 셋, 넷")
+    }
+}
