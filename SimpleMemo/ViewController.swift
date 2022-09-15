bfae3bbf95b8d46ccc765aa8f805bdd015326946
Taekyun Kim <taekkim@student.42seoul.kr>
2022-09-15 20:26:40 +0900

    Initial Commit

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by taekkim on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
